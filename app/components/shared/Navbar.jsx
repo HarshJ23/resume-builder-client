d8ac1a83d77743cc28b010b84e50c5f1974f08d1
HarshJ23 <hjawajiwar@gmail.com>
2024-01-25 01:44:51 +0530

    navbar edit

--- app/components/shared/Navbar.jsx
+++ app/components/shared/Navbar.jsx
@@ -2,12 +2,12 @@
 
 export default function Navbar() {
   return (
-    <div className="border-2 border-gray-600 p-2 w-1/2 rounded-full my-4 fixed top-0 overflow-hidden" >
-      <ul className="flex flex-row gap-12 justify-center font-semibold">
-        <li className="hover:bg-yellow-300 transition ease-in-out p-2 rounded-md hover:cursor-pointer">Home</li>
-        <li className="hover:bg-yellow-300 transition ease-in-out p-2 rounded-md hover:cursor-pointer">Plans</li>
-        <li className="hover:bg-yellow-300 transition ease-in-out p-2 rounded-md hover:cursor-pointer">Contact</li>
-        <li className="hover:bg-yellow-300 transition ease-in-out p-2 rounded-md hover:cursor-pointer">Resume Tips</li>
+    <div className="border-2 border-gray-600  w-1/2 rounded-full my-4 fixed top-0 overflow-hidden" >
+      <ul className="flex flex-row p-2 gap-12 justify-center font-semibold">
+        <li className="hover:bg-yellow-300 transition ease-in-out p-2 px-4   rounded-full hover:cursor-pointer">Home</li>
+        <li className="hover:bg-yellow-300 transition ease-in-out p-2 px-4 rounded-full hover:cursor-pointer">Career Blog</li>
+        <li className="hover:bg-yellow-300 transition ease-in-out p-2 px-4 rounded-full hover:cursor-pointer">Resources</li>
+        <li className="hover:bg-yellow-300 transition ease-in-out p-2 px-4 rounded-full hover:cursor-pointer">Resume Tips</li>
       </ul>
     </div>
 
